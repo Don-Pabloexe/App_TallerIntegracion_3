21d4c20299c83b63e59edbb5151fdefc4d31ff7b
FelipeArellanoV <farellano2021@alu.uct.cl>
2023-09-29 13:50:18 -0300

    Incorporacion Backend y prueba en Foro

---
+++ appTutor/src/complementos/Tutores.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { addDoc, collection, onSnapshot, query, orderBy } from 'firebase/firestore';
+import { db } from './firebaseConfig';
+
+const ChatScreen = () => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [username, setUsername] = useState('Usuario Anónimo');
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, 'messages'), orderBy('timestamp', 'asc')), // Mensajes en orden ascendente
+      (snapshot) => {
+        const fetchedMessages = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setMessages(fetchedMessages);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  const sendMessage = async () => {
+    if (newMessage.trim() !== '') {
+      try {
+        await addDoc(collection(db, 'messages'), {
+          text: newMessage,
+          username,
+          timestamp: new Date(),
+        });
+        setNewMessage('');
+      } catch (error) {
+        console.error('Error sending message:', error);
+      }
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <Text style={styles.title}>Chat Colaborativo</Text>
+      <FlatList
+        data={messages}
+        renderItem={({ item }) => (
+          <View style={styles.messageContainer}>
+            <Text style={styles.messageText}>{item.username}: {item.text}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.messagesList}
+      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Escribe un mensaje"
+          value={newMessage}
+          onChangeText={(text) => setNewMessage(text)}
+        />
+        <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
+          <Text style={styles.sendButtonText}>Enviar</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F0F0',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 16,
+    textAlign: 'center',
+  },
+  messagesList: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
+  },
+  messageContainer: {
+    backgroundColor: '#E3E3E3',
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 8,
+    maxWidth: '80%',
+  },
+  messageText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderTopWidth: 1,
+    borderTopColor: '#CCC',
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#007AFF',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+  },
+  sendButton: {
+    backgroundColor: '#007AFF',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    marginLeft: 8,
+  },
+  sendButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default ChatScreen;
